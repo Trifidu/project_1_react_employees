8e86562894f332bfe017a01a8b3b91f931e18e0e
Nick Dziubynskyi <nick.dzub@gmail.com>
2022-08-02 11:08:07 +0300

    add employees list

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,3 +1,5 @@
+import "./employees-list-item.css";
+
 const EmployeesListItem = () => {
   return (
     <li className="list-group-item d-flex justify-content-between">
@@ -7,11 +9,12 @@ const EmployeesListItem = () => {
         className="list-group-item-input"
         defaultValue="1000$"
       />
-      <div className="d-flex justify-content-center align-item-center">
-        <button type="button" className="btn-cookie btn-sm">
+      <div className="d-flex justify-content-center align-items-center">
+        <button type="button" className="btn-cookie btn-sm ">
           <i className="fas fa-cookie"></i>
         </button>
-        <button type="button" className="btn-trash btn-sm">
+
+        <button type="button" className="btn-trash btn-sm ">
           <i className="fas fa-trash"></i>
         </button>
         <i className="fas fa-star"></i>
